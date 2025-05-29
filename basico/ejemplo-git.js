662dfe8f3e0fb0863d9b6d6864a53747bb607c6d
Fer <fcastilloi@egresados.unp.edu.pe>
2025-05-29 15:31:47 -0500

    Fundamentos  de js

---
+++ basico/ejemplo-git.js
@@ -0,0 +1,10 @@
+// Este es un archivo de ejemplo para Git
+console.log("Aprendiendo Git!");
+
+// Ejemplo de una función
+function saludar() {
+    return "¡Hola desde Git!";
+}
+
+// Llamamos a la función
+console.log(saludar()); 
